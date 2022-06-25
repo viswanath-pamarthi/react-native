85d943f171100fdaf265209dae8d73ca7d131f68
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-25 01:41:39 -0400

    - Added AddItem component - Added ListItem component - Added logic to add a shopping list item and remove a shopping list item with empty validation and showing an alert

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,13 +1,42 @@
 //functional components with hooks
 
-import React from "react";
-import { View, Text, StyleSheet } from "react-native"
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList, Alert } from "react-native"
 import Header from "./components/Header";
+import uuid from "react-native-uuid";
+import ListItem from "./components/ListItem";
+import AddItem from "./components/AddItem";
 
 const App = () => {
+    const [items, setItems] = useState([
+        { id: uuid.v4(), text: 'Milk' },
+        { id: uuid.v4(), text: 'Eggs' },
+        { id: uuid.v4(), text: 'Bread' },
+        { id: uuid.v4(), text: 'Juice' }
+    ]);
+
+    const deleteItem = (id) => {
+        setItems((prevItems) => {
+            return prevItems.filter(item => item.id != id);
+        });
+    };
+    const addItem = (text) => {
+        if (!text) {
+            Alert.alert('Error', "Please enter an item", { text: 'Ok' })
+        } else {
+            setItems(prevItems => {
+                return [{ id: uuid.v4(), text: text }, ...prevItems]
+            });
+        }
+    };
     return (
         <View style={styles.container}>
             <Header title="Shopping List" />
+            <AddItem addItem={addItem} />
+            <FlatList data={items} renderItem={
+                ({ item }) => <ListItem item={item} deleteItem={deleteItem} />
+            }
+            />
         </View >
     );
 };
