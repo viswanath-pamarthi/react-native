2060acd663744d31ef03c67bf3397a82cb6580d7
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-24 23:56:08 -0400

    - added Stylesheet component to add styles for elements, refactored inline styles

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,14 +1,27 @@
 //functional components with hooks
 
 import React from "react";
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 const App = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ color: 'darkslateblue', fontSize: 30 }}>Hello world</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello world</Text>
         </View >
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'darkslateblue',
+        fontSize: 30,
+    }
+
+});
 
 export default App;
