a2c4a5ee335fe58b2d47b5e459a432c66d1c993a
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-27 15:34:22 -0400

    - Excluded manual vector icon resources included and let pod installation take care of it - Added new BoxScreen, HomeScreen

---
+++ ShoppingList/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
