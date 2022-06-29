8ef60e9066c5b220ef4205b3f4681bdb6a850efa
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-29 13:24:59 -0400

    - added a useReducer hook in BlogContext to improvize the code

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 //Using contexts to avoid pass a chain of data from parent to child screens( or a chind of child screens), with context(global state) we can pass the data to any child in deep navigation from context- here blogpovider
 //we will wrap app with blogContext here
 
 const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
 
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+        default:
+            return state;
+    }
+};
 /*
 example for children
 ----> here in similar syntax App component passes children:<Text>Hi there</Text> , the content inside CustomComponent tag is passed as a prop called "children"
@@ -18,15 +26,20 @@ const App=()=>{
 //we are not using export default, just export(a named export). we will do expor default for BlogContext
 //So inorder to import BlogProvider we need to use curly braces like import {BlogProvider}
 export const BlogProvider = ({ children }) => {
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);//useReducer will always returns state here blogPosts
     // const blogPosts = [
     //     { title: 'Blog Post #1' },
     //     { title: 'Blog Post #2' }
     // ];
 
-    const [blogPosts, setBlogPost] = useState([]);
+    //can also create  const editBlogPost = () => {, or to delete a post const addBlogPost = () => {, instead we can add a useReducer(this is a switch) hook to improve
+
+    // const [blogPosts, setBlogPost] = useState([]);
     //once addBlogPost(call back function) the blog posts are changed in useState/state variable, then Blog provider gets re render that makes application to rerender and it refreshes the list of blog posts sent to context and to index screen and to flat list 
     const addBlogPost = () => {
-        setBlogPost([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+        // setBlogPost([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+        dispatch({ type: 'add_blogpost' });
     }
 
     // return <BlogContext.Provider value={5}> this value= 5 with provider can be shared with with deep nested navigated child in the navigation stack. in other ways provider is the source for data
