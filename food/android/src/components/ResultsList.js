06b9adbd7d4aac29426e802f104598ab9efa67b4
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-28 23:48:26 -0400

    - Added results details component to show a restaurant in the list - wrapped search screen in scroll view to accommodate small screens

--- food/android/src/components/ResultsList.js
+++ food/android/src/components/ResultsList.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from "./resultsDetail";
 
 const ResultsList = ({ title, results }) => {
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList horizontal //short hand of horizontal ={true}
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={(result) => result.id}
                 renderItem={({ item }) => {
-                    return <Text>{item.name}</Text>
+                    return <ResultsDetail result={item} />
                 }}
             />
         </View>);
@@ -19,7 +21,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: "bold"
+        fontWeight: "bold",
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
--- food/android/src/screens/SearchScreen.js
+++ food/android/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
@@ -21,9 +21,11 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text >{errorMessage}</Text> : null}
             <Text> We have found {results.length} results</Text>
-            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-            <ResultsList results={filterResultsByPrice('$$')} title="Bit  Pricier" />
-            <ResultsList results={filterResultsByPrice('$$$')} title="Big  Spender" />
+            <ScrollView>
+                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+                <ResultsList results={filterResultsByPrice('$$')} title="Bit  Pricier" />
+                <ResultsList results={filterResultsByPrice('$$$')} title="Big  Spender" />
+            </ScrollView>
         </View>
     );
 }
