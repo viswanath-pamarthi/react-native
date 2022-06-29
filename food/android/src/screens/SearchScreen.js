62d1d7e2f2e8bbfe4847bd9e771295e109633592
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-28 22:55:19 -0400

    - refactored part of code in Search Screen to a useResults hook

--- food/android/src/screens/SearchScreen.js
+++ food/android/src/screens/SearchScreen.js
@@ -1,35 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import Icon from "react-native-vector-icons/dist/FontAwesome";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState('');
-
-    const searchApi = async () => {
-        // console.log(term);
-
-        const response = await yelp.get('/search', {
-            params: {//query strings
-                limit: 50,
-                // term: term
-                term,//short hand as api and the variable are same name
-                location: "san jose"
-            }
-        });
-        setResults(response.data.businesses);
-    };
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View style={styles.viewStyle}>
             <SearchBar term={term}
-                onTermChange={(newterm) => { setTerm(newterm) }}
-                onTermSubmit={searchApi} />{/* shorthand for ->....() => { searchApi() }*/}
-            {/* <Icon name="search" size={30} > */}
-            <Text >Search Screen</Text>
-            {/* </Icon> */}
+                onTermChange={setTerm} //{/* shorthand for ->..(newterm) => { setTerm(newterm) }*/}
+                onTermSubmit={() => searchApi(term)} />
+            {errorMessage ? <Text >{errorMessage}</Text> : null}
             <Text> We have found {results.length}</Text>
         </View>
     );
