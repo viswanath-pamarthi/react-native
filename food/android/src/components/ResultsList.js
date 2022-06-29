69e7844c79cce0064a6fb56568e2e0639a4f2073
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-06-28 23:21:17 -0400

    - Added Results List component to display each section of restaurants - filtered search result to each section of restaurants - Cost effective, Bit pricer, Big Spender

---
+++ food/android/src/components/ResultsList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ResultsList = ({ title, results }) => {
+
+    return (
+        <View>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList horizontal //short hand of horizontal ={true}
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={({ item }) => {
+                    return <Text>{item.name}</Text>
+                }}
+            />
+        </View>);
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: "bold"
+    }
+});
+
+export default ResultsList;
--- food/android/src/screens/SearchScreen.js
+++ food/android/src/screens/SearchScreen.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import Icon from "react-native-vector-icons/dist/FontAwesome";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+    const filterResultsByPrice = (price) => {
+        //price === '$ || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
 
+    };
     return (
         <View style={styles.viewStyle}>
             <SearchBar term={term}
                 onTermChange={setTerm} //{/* shorthand for ->..(newterm) => { setTerm(newterm) }*/}
                 onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text >{errorMessage}</Text> : null}
-            <Text> We have found {results.length}</Text>
+            <Text> We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit  Pricier" />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big  Spender" />
         </View>
     );
 }
