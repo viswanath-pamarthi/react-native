a35162cc18d475421f42efc9aa2ce650aae156af
Viswanath Pamarthi <Viswanath.Pamarthi@outlook.com>
2022-07-07 16:46:00 -0400

    - Create a JWT token on SignUp endpoint once SignUp is successful and return it

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
@@ -9,7 +10,9 @@ router.post('/signup', async (req, res) => {
     try {
         const user = new User({ email: email, password: password });
         await user.save();//Save user to Mongo Db hosted instance, if the user collection desn't exist on mongodb , Mongoose will create a new collection in MondoDb
-        res.send('You made a post request');
+
+        const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");//generate JWT token
+        res.send({ token: token });
     }
     catch (err) {
         return res.status(422).send(err.message);//422 status code, user sent us invalid data
